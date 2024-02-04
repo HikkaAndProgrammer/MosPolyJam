3160b8eca1f8de22e016eb7b291b2427600f6702
SanyaTravka <alextr663@gmail.com>
2024-02-05 02:48:31 +0300

    Меню

---
+++ Assets/Scripts/MenuButtonsScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MenuButtonsScript : MonoBehaviour
+{
+    public GameObject levels_panel;
+
+    public void ButtonStart()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+        levels_panel.SetActive(!levels_panel.activeSelf);
+    }
+
+    public void ButtonExit()
+    {
+        Application.Quit();
+    }
+    public void ButtonJam()
+    {
+        Application.OpenURL("https://itch.io/jam/mospolyjam-2");
+    }
+}
---
+++ Assets/Scripts/SetLevelScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SetLevelScript : MonoBehaviour
+{
+    public void StartScene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+}
