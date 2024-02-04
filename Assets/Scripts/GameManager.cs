bf2b7e8a3328585f6981774015c5e2430af9e7f5
AlexToshin <145476408+AlexToshin@users.noreply.github.com>
2024-02-04 21:28:39 +0300

    dfghjk

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Transform player;
+    private Vector3 startPos;
     public static GameManager gameManager;
     public bool isMoving = false;
     // Start is called before the first frame update
@@ -11,6 +14,8 @@ public class GameManager : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
         gameManager =this;
+        startPos = player.position;
+
     }
 
     // Update is called once per frame
@@ -27,5 +32,7 @@ public class GameManager : MonoBehaviour
     public void StopTheRun()
     {
         isMoving=false;
+        player.position = startPos;
+        
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float elapsedTime = 0;
+
     [SerializeField] private float stepDuration = 1f;
     [SerializeField] private float tileSize = 1f;
     [SerializeField] private string[] path;
     //[SerializeField] private AnimationCurve curve;
     private Vector3 directionMove;
-    private float percantage = 0;
-    private int currentDirIndex = 0;
+    public int currentDirIndex = 0;
     private Vector3 startPos;
     /// <summary>
     ///private bool playerIsMoving = false;
@@ -22,9 +21,12 @@ public class PlayerMovement : MonoBehaviour
         if(GameManager.gameManager.isMoving){
             Move();
         }
+        else
+        {
+           currentDirIndex = 0;
+        }
     }
     void Start(){
-        elapsedTime = 0;
         startPos = transform.position;
     }
     void Move(){
